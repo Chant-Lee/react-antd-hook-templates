00ab18b2566e6665f5a4ab561d7d59b8c82b5b93
LiangquanLi <liangquan.li@shopee.com>
2020-09-10 11:23:35 +0800

    feat: add use page hook

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { usePage } from '@/components/page';
 
 const styles = require('./index.module.less');
 
 const Home = () => {
-  return (
+  const [fragment, renderPage, withLoading] = usePage();
+
+  fragment.title = <span>Home Page</span>;
+  fragment.content = withLoading(() => (
     <div className={styles.wrapper}>
       <div className={styles.wrapperInfo}>5555</div>
     </div>
-  );
+  ));
+  return <div>{renderPage()}</div>;
 };
 
 export default Home;
